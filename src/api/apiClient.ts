126627b76eb85c7eda4c83a036f1bea0f16a5467
taehee <jangth0655@gmail.com>
2022-11-07 10:19:06 +0900

    [fix] apiclient 필요없는 주석 정리

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,33 +1,5 @@
 import axios from 'axios';
 
-type Params = {
-  'X-Amz-Algorithm': 'AWS4-HMAC-SHA256';
-  'X-Amz-Content-Sha256': 'UNSIGNED-PAYLOAD';
-  'X-Amz-Credential': 'AKIAT73L2G45EIPT3X45%2F20221105%2Fus-west-2%2Fs3%2Faws4_request';
-  'X-Amz-Date': '20221105T155302Z';
-  'X-Amz-Expires': 86400;
-  'X-Amz-Signature': 'e13d0853b6e40d62fd3338f40edd680be3cf7b848fb42353c839da6d63b3f8dd';
-  'X-Amz-SignedHeaders': 'host';
-  'response-content-disposition': 'filename%3D%22wanted_FE_trend-data-set.json%22&x-id=GetObject';
-};
-
-const params: Params = {
-  'X-Amz-Algorithm': 'AWS4-HMAC-SHA256',
-  'X-Amz-Content-Sha256': 'UNSIGNED-PAYLOAD',
-  'X-Amz-Credential':
-    'AKIAT73L2G45EIPT3X45%2F20221105%2Fus-west-2%2Fs3%2Faws4_request',
-  'X-Amz-Date': '20221105T155302Z',
-  'X-Amz-Expires': 86400,
-  'X-Amz-Signature':
-    'e13d0853b6e40d62fd3338f40edd680be3cf7b848fb42353c839da6d63b3f8dd',
-  'X-Amz-SignedHeaders': 'host',
-  'response-content-disposition':
-    'filename%3D%22wanted_FE_trend-data-set.json%22&x-id=GetObject',
-};
-
-const baseURL =
-  'https://s3.us-west-2.amazonaws.com/secure.notion-static.com/87bdcbe0-76c9-4df3-a3b0-efb2980f66b5/wanted_FE_trend-data-set.json?';
-
 class ApiClient {
   // eslint-disable-next-line class-methods-use-this
   getTrendList = async () => {
